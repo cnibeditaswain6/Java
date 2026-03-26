f800b8f67fb08195e6f39ae83f78abe23f8e295b
C Nibedita Swain <nibeditaswain351@gmail.com>
2026-03-26 14:45:50 +0530

    Add files via upload

---
+++ DemoOperator.java
@@ -0,0 +1,16 @@
+class DemoOperator
+{
+	public static void main(String[] args) 
+	{
+		int a = 7 + 2;
+		int b = 7 - 2;
+		int c = 7 * 2;
+		int d = 7 / 2;
+		int e = 7 % 2;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
