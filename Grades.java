a71a82366ad63debd05aa601887ba8a5cb7320d2
C Nibedita Swain <nibeditaswain351@gmail.com>
2026-03-30 21:30:16 +0530

    Add files via upload

---
+++ Grades.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Grades 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Mark : ");
+		int mark = sc.nextInt();
+		
+		if (mark >= 90)
+		{
+			System.out.println("Grade : A ");
+		}
+		else if (mark >= 75)
+		{
+			System.out.println("Grade : B ");
+		}
+		else if (mark >= 60)
+		{
+			System.out.println("Grade : C ");
+		}
+		else if (mark >= 40)
+		{
+			System.out.println("Grade : D ");
+		}
+		else
+		{
+			System.out.println("Fail");
+		}
+	}
+}
