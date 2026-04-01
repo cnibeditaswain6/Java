2aab409a45b2dbc86e21ccf995c8fc8d0a896219
C Nibedita Swain <nibeditaswain351@gmail.com>
2026-04-01 08:32:13 +0530

    Add files via upload

---
+++ Identifiers.java
@@ -0,0 +1,10 @@
+class Identifiers
+{
+	public static void main(String[] args) 
+	{
+		System.out.println('a' + 0);
+		System.out.println('Z' + 0);
+		System.out.println('A' + 0);
+		System.out.println('z' + 0);
+	}
+}
