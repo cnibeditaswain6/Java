dcf8587d883a5783c88475fb77688de99be5167e
C Nibedita Swain <nibeditaswain351@gmail.com>
2026-04-02 08:33:22 +0530

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,11 @@
+//Write a Program to display numbers from 1 to 10 on separete lines,using for loop.
+class Display
+{
+	public static void main(String[] args) 
+	{	
+		for (int i=1; i<=10 ; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
